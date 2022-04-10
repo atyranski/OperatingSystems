87ed714c03ce573d79dd5aae54d57f2de82eca82
Arkadiusz Tyrański <72259657+atyranski@users.noreply.github.com>
2022-04-10 22:20:23 +0200

    [10.04.2022: 22:20]

--- lab5/zad1/src/main.c
+++ lab5/zad1/src/main.c
@@ -15,17 +15,22 @@ typedef struct {
     char *name;
     char **comands;
     int commands_amount;
+    int capacity;
 } Component;
 
 typedef struct {
     char **components_names;
     int names_amount;
+    int capacity;
 } Sequence;
 
 typedef struct {
     Component **components;
-    Sequence **sequences;
+    int components_amount;
     int secquences_amount;
+    Sequence **sequences;
+    int components_capacity;
+    int secquences_capacity;
 } Program;
 
 
@@ -76,46 +81,70 @@ char *getFileContent(FILE *file){
 void printComponent(Component *component){
     printCheck("Component");
     printf("[name]: %s\n", component->name);
-    printf("[commands]: \n\t> %s", component->comands[0]);
-    if(component->comands[1] != NULL){
-        printf("\n\t> %s", component->comands[1]);
-
-        if(component->comands[2] != NULL){
-            printf("\n\t> %s\n", component->comands[2]);
-        } else {
-            printf("\n");
-        }
-
-    } else {
-        printf("\n");
-    }
-    printf("[commands_amount]: %d\n\n", component->commands_amount);
+    printf("[commands]: \n");
+    for(int i=0; i<component->commands_amount; i++) printf("\t> %s\n", component->comands[i]);
+    printf("[amount]: %d\n", component->commands_amount);
+    printf("[capacity]: %d\n\n", component->capacity);
 }
 
 void printSequence(Sequence *sequence){
     printCheck("Sequence");
-    printf("[components_names]: \n\t> %s", sequence->components_names[0]);
-    if(sequence->components_names[1] != NULL){
-        printf("\n\t> %s", sequence->components_names[1]);
+    printf("[components_names]: \n");
+    for(int i=0; i<sequence->names_amount; i++) printf("\t> %s\n", sequence->components_names[i]);
+    printf("[amount]: %d\n", sequence->names_amount);
+    printf("[capacity]: %d\n\n", sequence->capacity);
+}
 
-        if(sequence->components_names[2] != NULL){
-            printf("\n\t> %s\n", sequence->components_names[2]);
-        } else {
-            printf("\n");
-        }
+void printProgram(Program *program){
+    // printComponent(program->components[0]);
+    printInfo("Printing", "program");
+    for(int i=0; i<program->components_amount; i++) printComponent(program->components[i]);
+    for(int i=0; i<program->secquences_amount; i++) printSequence(program->sequences[i]);
+}
 
-    } else {
-        printf("\n");
-    }
-    printf("[names_amount]: %d\n\n", sequence->names_amount);
+void enlargeComponent(Component *component){
+    component->capacity *= 2;
+    component->comands = realloc(component->comands, component->capacity*50);
+}
+
+void enlargeSequence(Sequence *sequence){
+    sequence->capacity *= 2;
+    sequence->components_names = realloc(sequence->components_names, sequence->capacity*50);
+}
+
+void enlargeProgramComponents(Program *program){
+    program->components_capacity *= 2;
+    program->components = realloc(program->components, program->components_capacity);
+}
+
+void enlargeProgramSequences(Program *program){
+    program->secquences_capacity *= 2;
+    program->sequences = realloc(program->sequences, program->secquences_capacity);
+}
+
+void freeComponent(Component *component){
+    free(component->name);
+    free(component->comands);
+    free(component);
+}
+
+void freeSequence(Sequence *sequence){
+    free(sequence->components_names);
+    free(sequence);
+}
+
+void freeProgram(Program *program){
+    for(int i=0; i<program->components_amount; i++) freeComponent(program->components[i]);
+    for(int i=0; i<program->secquences_amount; i++) freeSequence(program->sequences[i]);
 }
 
 Component *parseComponent(char *line){
     printInfo("Parsing Component", line);
 
-    Component *newComponent = malloc(sizeof(Component));
+    Component *newComponent = calloc(1, sizeof(Component));
     // assumption that any component can handle max 3 commands of length 50 characters each
     newComponent->comands = calloc(3*50, sizeof(char));
+    newComponent->capacity = 3;
 
     // Getting component name
     char *start, *end, *name;
@@ -154,14 +183,15 @@ Component *parseComponent(char *line){
             name[i] = *(start + i);
         }
 
+        if(i == newComponent->capacity) enlargeComponent(newComponent);
+        newComponent->commands_amount = i;
         newComponent->comands[i] = name;
         i++;
-
     }
-
+    
     newComponent->commands_amount = i;
 
-    printComponent(newComponent);
+    // printComponent(newComponent);
 
     return newComponent;
 }
@@ -169,9 +199,10 @@ Component *parseComponent(char *line){
 Sequence *parseSequence(char *line){
     printInfo("Parsing Sequence", line);
 
-    Sequence *newSequence = malloc(sizeof(Sequence));
+    Sequence *newSequence = calloc(1, sizeof(Sequence));
     // assumption that any component can handle max 3 commands of length 50 characters each
     newSequence->components_names = calloc(3*50, sizeof(char));
+    newSequence->capacity = 3;
 
     char *start, *end, *name;
     int lenght;
@@ -193,25 +224,26 @@ Sequence *parseSequence(char *line){
             name[i] = *(start + i);
         }
 
+        if(i == newSequence->capacity) enlargeSequence(newSequence);
+        newSequence->names_amount = i;
         newSequence->components_names[i] = name;
         i++;
-
         line++;
-
     }
 
     newSequence->names_amount = i;
 
-    printSequence(newSequence);
+    // printSequence(newSequence);
 
+    return newSequence;
 }
 
-
-void readLines(char *text){
+void readLines(char *text, Program *program){
     printInfo("Initialization", "reading lines");
 
     char *start, *end;
     bool isComponent = true;
+    int index = 0;
 
     while(*text != 0){
         start = text;
@@ -224,7 +256,7 @@ void readLines(char *text){
         end = text;
 
         int lenght = end - start-1;
-        char *line = malloc(lenght * sizeof(char));
+        char *line = calloc(lenght, sizeof(char));
 
         for(int i=0; i<lenght; i++){
             line[i] = *(start + i);
@@ -232,24 +264,50 @@ void readLines(char *text){
 
         if(lenght == 0) {
             isComponent = false;
+            index = 0;
             continue;
         }
 
-        if(isComponent) parseComponent(line);
-        else parseSequence(line);
+        if(isComponent) {
+            Component *newComponent = parseComponent(line);
+
+            if(program->components_capacity == index) enlargeProgramComponents(program);
+
+            program->components[index] = newComponent;
+            program->components_amount++;
+
+        } else {
+            Sequence *newSequecnce = parseSequence(line);
+
+            if(program->secquences_capacity == index) enlargeProgramSequences(program);
+
+            program->sequences[index] = newSequecnce;
+            program->secquences_amount++;
+
+        }
 
         free(line);
+        index++;
     }
     
 }
 
-Program *parseFile(FILE *file){
+Program *parseProgram(FILE *file){
     printInfo("Initialization", "start program");
-    Program *program = malloc(sizeof(Program));
+    Program *program = calloc(1, sizeof(Program));
+    program->components_capacity = 3;
+    program->components_amount = 0;
+    program->components = calloc(program->components_capacity, sizeof(Component));
+    program->secquences_capacity = 3;
+    program->secquences_amount = 0;
+    program->sequences = calloc(program->secquences_capacity, sizeof(Sequence));
+
     char *file_content = getFileContent(file);
 
     // printf("%s\n", file_content);
-    readLines(file_content);
+    readLines(file_content, program);
+
+    printProgram(program);
 
     return program;
 }
@@ -271,11 +329,10 @@ int main(int argc, char **argv){
     if(file == NULL) return RETURN_COULDNT_OPEN_FILE;
 
     // Get data from file and put into structs
-    program = parseFile(file);
-
+    program = parseProgram(file);
     
     close(file);
-    free(program);
+    freeProgram(program);
 
     return RETURN_SUCCESS;
 }
